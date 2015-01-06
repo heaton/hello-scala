88df46cc2766ab643bf74a331a3cbb1f29dfb3b4
Heaton Cai <heaton.cai@gmail.com>
2015-01-06 20:37:06 +0800

    polynomial foldLeft version in class 6.6

--- src/main/scala/me/heaton/profun/week6/polynomial.scala
+++ src/main/scala/me/heaton/profun/week6/polynomial.scala
@@ -6,17 +6,19 @@ package me.heaton.profun.week6
 object polynomial {
 
   class Poly(terms0: Map[Int, Double]) {
+    def this(bindings: (Int, Double)*) = this(bindings.toMap)
     val terms = terms0 withDefaultValue 0.0
-    def + (other: Poly) = new Poly(terms ++ (other.terms map adjust))
-    def adjust(term: (Int, Double)): (Int, Double) = {
+
+    def + (other: Poly) = new Poly((other.terms foldLeft terms)(addTerm))
+    def addTerm(terms: Map[Int, Double], term: (Int, Double)): Map[Int, Double] = {
       val (exp, coeff) = term
-      exp -> (coeff + terms(exp))
+      terms + (exp -> (coeff + terms(exp)))
     }
 
     override def toString =
       (for ((exp, coeff) <- terms.toList.sorted.reverse) yield coeff + "x^" + exp) mkString " + " replaceAll ("\\.0+x", "x") replaceAll ("x\\^0", "")
   }
 
-  val p1 = new Poly(Map(1 -> 2.0, 3 -> 4.0, 5 -> 6.2))
+  val p1 = new Poly(1 -> 2.0, 3 -> 4.0, 5 -> 6.2)
   val p2 = new Poly(Map(0 -> 3.0, 3 -> 7.0))
 }
