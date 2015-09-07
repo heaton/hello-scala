0a1a13012c2ee01c6d393c5d4286a0eee6423244
Heaton Cai <heaton.cai@gmail.com>
2015-09-07 14:01:55 +0800

    update test

--- src/test/scala/me/heaton/spec2/SomeSpec.scala
+++ src/test/scala/me/heaton/spec2/SomeSpec.scala
@@ -1,13 +1,12 @@
 package me.heaton.spec2
 
-import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 
-class SomeSpec extends Specification with Mockito{
+class SomeSpec extends Specification {
 
-  "1+2" should {
-    "= 3" in {
-      1 + 2 === 3
+  "Heaton" should {
+    "say hello to you" in {
+      (List("Hello", "there!") mkString " ") === "Hello there!"
     }
   }
 
