afd5e9c3f1821a355edf502f7dd803b095826017
Heaton Cai <heaton.cai@gmail.com>
2015-09-21 16:53:05 +0800

    update specs

--- src/test/scala/me/heaton/spec2/SomeSpec.scala
+++ src/test/scala/me/heaton/spec2/SomeSpec.scala
@@ -5,10 +5,17 @@ import org.specs2.mock.Mockito
 import org.specs2.mutable._
 import org.specs2.specification.Scope
 
+/**
+ * https://etorreborre.github.io/specs2/guide/SPECS2-2.4.17/org.specs2.guide.Structure.html
+ */
 class SomeSpec extends Specification with Mockito {
 
+  step {
+    "first setp".pp
+  }
+
   "Heaton" should {
-    "say hello to you" in{
+    "say hello to you" in {
       (List("Hello", "there!") mkString " ") === "Hello there!"
     }
   }
@@ -25,7 +32,7 @@ class SomeSpec extends Specification with Mockito {
         numbers must contain(1)
         numbers must not contain 4
 
-        numbers must containAllOf(List(1, 2, 3))
+        numbers must containAllOf(List(2, 1, 3))
       }
 
       "and other tests" in {
@@ -36,6 +43,7 @@ class SomeSpec extends Specification with Mockito {
       "and options" in {
         Some("Heaton") must beSome[String]
         Some("Heaton") must beSome("Heaton")
+        None must beNone
       }
 
       //a must throwA[WhateverException]
@@ -47,7 +55,8 @@ class SomeSpec extends Specification with Mockito {
 
     "let us create our own Matcher" in {
       case class SumOf(a: Int) extends Matcher[List[Int]] {
-        def apply[S <: List[Int]](x: Expectable[S]) = result(x.value.sum == a, "OK", "Not OK", x)
+        def apply[S <: List[Int]](x: Expectable[S]) =
+          result(x.value.sum == a, "OK", "Not OK", x)
       }
       List(1,2,3) must SumOf(6)
     }
@@ -58,12 +67,38 @@ class SomeSpec extends Specification with Mockito {
         def get(i: Int): T
       }
 
-      val m = mock[AClass[String]]
-      m.get(0) returns "one"
-      m.get(0) === "one"
+      "nomarl mocks" in {
+        val m = mock[AClass[String]]
 
-      there was one(m).get(0)
-      there was no(m).get(1)
+        m.get(0) returns "one"
+        m.get(0) === "one"
+
+        there was one(m).get(0)
+        there was no(m).get(1)
+      }
+
+      "try spy" in {
+        class AnotherClass extends AClass[String] {
+          override def get(i: Int): String = i.toString
+        }
+        val c = new AnotherClass
+
+        c.get(1) === "1"
+
+        val c2 = spy(c)
+        c2.get(0) returns "zero"
+        c2.get(0) === "zero"
+      }
+
+      "try spy object" in {
+        object AObject extends AClass[String] {
+          override def get(i: Int): String = i.toString
+        }
+
+        val o = spy(AObject)
+        o.get(0) returns "zero"
+        o.get(0) === "zero"
+      }
     }
 
     "has isolation with Scope" in new aScope {
@@ -75,6 +110,10 @@ class SomeSpec extends Specification with Mockito {
       1 === 1
     }
 
+  }
+
+  step {
+    "last setp".pp
   }
 
   trait aScope extends Scope {
